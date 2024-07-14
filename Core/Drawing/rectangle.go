beeb898c50c7a253cb1443ef3b8a5dc71f52a594
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-07-14 21:51:23 +0200

    refactor: property visibility

--- Core/Drawing/rectangle.go
+++ Core/Drawing/rectangle.go
@@ -9,23 +9,23 @@ import (
 type Rectangle struct {
 	isChanged bool
 	ansiCode  string
-	color     Color.Color
+	Color     Color.Color
 	name      string
-	xPos      int
-	yPos      int
-	width     int
-	height    int
+	XPos      int
+	YPos      int
+	Width     int
+	Height    int
 }
 
 func CreateRectangle(name string,x, y, width, height int) *Rectangle {
 	return &Rectangle{
 		ansiCode: "",
 		name:     name,
-		xPos:     x,
-		yPos:     y,
-		width:    width,
+		XPos:     x,
+		YPos:     y,
+		Width:    width,
 		isChanged:    true,
-		height:   height,
+		Height:   height,
 	}
 }
 
@@ -44,32 +44,26 @@ func (r *Rectangle) GetAnsiCode() string {
 func (s *Rectangle) GetName() string {
 	return s.name
 }
-func (s *Rectangle) SetColor(c Color.Color) {
-	s.color = c
-}
-func (s *Rectangle) GetColor() Color.Color {
-	return s.color
-}
 func (s *Rectangle) getAnsiRectangle() string {
-	var horizontal = strings.Repeat(U.HorizontalLine, s.width-2)
+	var horizontal = strings.Repeat(U.HorizontalLine, s.Width-2)
 	var builStr strings.Builder
 	builStr.WriteString(U.SaveCursor)
-	builStr.WriteString(U.GetAnsiMoveTo(s.xPos, s.yPos))
+	builStr.WriteString(U.GetAnsiMoveTo(s.XPos, s.YPos))
 	builStr.WriteString(U.TopLeftCorner)
 	builStr.WriteString(horizontal)
-	for i := 1; i < s.height-1; i++ {
-		builStr.WriteString(U.GetAnsiMoveTo(s.xPos, s.yPos+i))
+	for i := 1; i < s.Height-1; i++ {
+		builStr.WriteString(U.GetAnsiMoveTo(s.XPos, s.YPos+i))
 		builStr.WriteString(U.VerticalLine)
 	}
-	builStr.WriteString(U.GetAnsiMoveTo(s.xPos, s.yPos+s.height-1))
+	builStr.WriteString(U.GetAnsiMoveTo(s.XPos, s.YPos+s.Height-1))
 	builStr.WriteString(U.BottomLeftCorner)
 	builStr.WriteString(horizontal)
 	builStr.WriteString(U.BottomRightCorner)
-	for i := 1; i < s.height-1; i++ {
-		builStr.WriteString(U.GetAnsiMoveTo(s.xPos+s.width-1, s.yPos+i))
+	for i := 1; i < s.Height-1; i++ {
+		builStr.WriteString(U.GetAnsiMoveTo(s.XPos+s.Width-1, s.YPos+i))
 		builStr.WriteString(U.VerticalLine)
 	}
-	builStr.WriteString(U.GetAnsiMoveTo(s.xPos+s.width-1, s.yPos))
+	builStr.WriteString(U.GetAnsiMoveTo(s.XPos+s.Width-1, s.YPos))
 	builStr.WriteString(U.TopRightCorner)
 	builStr.WriteString(U.RestoreCursor)
 	return builStr.String()
--- Core/IEntity.go
+++ Core/IEntity.go
@@ -1,11 +1,8 @@
 package Core
 
-import "GTUI/Core/Color"
 
 type IEntity interface {
 	Touch()
 	GetAnsiCode() string
 	GetName() string
-	SetColor(color Color.Color)
-	GetColor() Color.Color
 }
