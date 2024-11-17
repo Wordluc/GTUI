2d078e72f36c3d5243538a0d51908a578092b4d9
wordluc <frangiamoreluca@gmail.com>
2024-11-17 12:03:57 +0100

    refactor: test eventmanager

--- Core/EventManager/eventManager_test.go
+++ Core/EventManager/eventManager_test.go
@@ -4,38 +4,41 @@ import (
 	"testing"
 	"time"
 )
-
+const (
+	test1 EventType=9
+	test2 EventType=10
+	test3 EventType = iota
+)
+type callerForTest struct {
+	name string
+}
 func testTime(t time.Time, timeToWait int) (int, bool) {
 	res := (time.Now().UnixNano() - t.UnixNano()) / int64(time.Millisecond)
 	return int(res), int(res) == timeToWait
 }
-func _TestEventManager(t *testing.T) {
+func TestEventManager(t *testing.T) {
 	Setup()
 	var isok bool
 	var res int
 	timer := time.Now()
 
-	Subscribe(Refresh, 2, func(comp []any) {
+	Subscribe(test1, 2, func(comp []any) {
 		res, isok = testTime(timer, 2)
 	})
-	Call(Refresh, nil)
+	Call(test1, nil)
 	time.Sleep(100 * time.Millisecond)
 	if !isok {
 		t.Errorf("expected to wait 2, got %v", res)
 	}
 }
-type callerForTest struct {
-	name string
-}
-func _TestEventManagerWithMultipeCall(t *testing.T) {
-	delete(eventManager.subscribers,Refresh)
+func TestEventManagerWithMultipeCall(t *testing.T) {
 	var timer time.Time
 	var isok bool
 	var ncalls int
 	var res int
 	var caller string
 	timer = time.Now()
-	e:=Subscribe(Refresh, 500, func(comp []any) {
+	e:=Subscribe(test2, 500, func(comp []any) {
 		ncalls++
 		res, isok = testTime(timer, 500)
 		for _,c:=range comp{
@@ -48,11 +51,11 @@ func _TestEventManagerWithMultipeCall(t *testing.T) {
 	}
 	callerA:=callerForTest{"a"}
 	callerB:=callerForTest{"b"}
-	if e:=Call(Refresh, []any{callerA});e!=nil{
+	if e:=Call(test2, []any{callerA});e!=nil{
 		t.Errorf("expected no error,got %v", e)
 	}
 	time.Sleep(100 * time.Millisecond)
-	Call(Refresh, []any{callerB})
+	Call(test2, []any{callerB})
 	time.Sleep(500 * time.Millisecond)
 	if !isok {
 		t.Errorf("expected to wait 2,got %d",res)
@@ -64,11 +67,10 @@ func _TestEventManagerWithMultipeCall(t *testing.T) {
 		t.Errorf("expected ab|,got %s", caller)
 	}
 }
-func _TestEventManagerWithDistantMultipeCall(t *testing.T) {
-	delete(eventManager.subscribers,Refresh)
+func TestEventManagerWithDistantMultipeCall(t *testing.T) {
 	var ncalls int
 	var caller string
-	e:=Subscribe(Refresh, 300, func(comp []any) {
+	e:=Subscribe(test3, 300, func(comp []any) {
 		ncalls++
 		for _,c:=range comp{
 			caller+=c.(callerForTest).name
@@ -80,22 +82,17 @@ func _TestEventManagerWithDistantMultipeCall(t *testing.T) {
 	}
 	callerA:=callerForTest{"a"}
 	callerB:=callerForTest{"b"}
-	if e:=Call(Refresh, []any{callerA});e!=nil{
+	if e:=Call(test3, []any{callerA});e!=nil{
 		t.Errorf("expected no error,got %v", e)
 	}
 	time.Sleep(550 * time.Millisecond)
-	Call(Refresh, []any{callerB})
-	time.Sleep(500 * time.Millisecond)
+	Call(test3, []any{callerB})
+	time.Sleep(400 * time.Millisecond)
 	if ncalls != 2 {
 		t.Errorf("expected 2 calls,got %d", ncalls)
 	}
 	if caller!="a|b|"{
 		t.Errorf("expected a|b|,got %s", caller)
 	}
+}
 
-}
-func TestEventManagerSequentially(t *testing.T) {
-	_TestEventManager(t)
-	_TestEventManagerWithMultipeCall(t)
-	_TestEventManagerWithDistantMultipeCall(t)
-}
