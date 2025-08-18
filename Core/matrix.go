125bf844a1e7aff2d8db64cdaea02896df68ee32
Wordluc <frangiamoreluca@gmail.com>
2025-08-18 22:42:22 +0200

    fix: add element and search,it requires optimization

--- Core/matrix.go
+++ Core/matrix.go
@@ -14,6 +14,7 @@ type matrix[T any] [][][]MatrixElement[ElementTree] //rows,columns,elements
 type MatrixElements struct {
 	matrix                    matrix[MatrixElement[ElementTree]]
 	nColumns, nRows, sizeCell int
+	elements                  []ElementTree
 }
 
 func CreateMatrixElements(nColumns, nRows, size int) *MatrixElements {
@@ -38,25 +39,25 @@ func divideCeil(a, b int) int {
 // start thinking how to link stuff together, the objects have to know that on the right there are some objects ecc....
 // so maybe use a wrapper for ElementTree, where i can store information about my neighbour
 func (m *MatrixElements) addElement(element ElementTree) {
-
+	m.elements = append(m.elements, element)
 	x, y := element.GetPos()
 	width, height := element.GetSize()
 
 	startingColumn := x / m.sizeCell
 	startingRow := y / m.sizeCell
 
-	column := divideCeil(width, m.sizeCell)
-	row := divideCeil(height, m.sizeCell)
+	column := width / m.sizeCell
+	row := height / m.sizeCell
 
 	for ir := range row + 1 {
 		for iw := range column + 1 {
+			currentRow, currentColumn := ir+startingRow, iw+startingColumn
 			if ir+startingRow >= m.nRows {
 				continue
 			}
 			if iw+startingColumn >= m.nColumns {
 				continue
 			}
-			currentRow, currentColumn := ir+startingRow, iw+startingColumn
 			m.matrix[currentRow][currentColumn] =
 				append(m.matrix[currentRow][currentColumn], MatrixElement[ElementTree]{object: element})
 		}
@@ -91,7 +92,6 @@ func (m *MatrixElements) search(x, y int) (res []ElementTree) {
 }
 
 type MatrixHandler struct {
-	elements               []ElementTree
 	layers                 []*MatrixElements
 	nCollumns, nRows, size int
 }
@@ -99,8 +99,8 @@ type MatrixHandler struct {
 // TODO: i have to pass the size of the screen and from that compute the w and h
 func CreateMatrixHandler(w, h, sizeCell int) *MatrixHandler {
 	return &MatrixHandler{
-		nCollumns: divideCeil(w, sizeCell),
-		nRows:     divideCeil(h, sizeCell),
+		nCollumns: w / sizeCell,
+		nRows:     h / sizeCell,
 		size:      sizeCell,
 	}
 }
@@ -121,11 +121,10 @@ func (m *MatrixHandler) AddElement(eles ...ElementTree) {
 			m.layers = append(m.layers, nil)
 		}
 		if m.layers[layer] == nil {
-			m.layers[layer] = CreateMatrixElements(m.nCollumns, m.nRows, 10)
+			m.layers[layer] = CreateMatrixElements(m.nCollumns, m.nRows, m.size)
 
 		}
 		m.layers[layer].addElement(ele)
-		m.elements = append(m.elements, ele)
 	}
 }
 
@@ -133,24 +132,59 @@ func (m *MatrixHandler) GetLayerN() int {
 	return len(m.layers)
 }
 
-func (m *MatrixHandler) Refresh() {
-	handler := CreateMatrixHandler(m.nCollumns, m.nRows, m.size)
-	handler.AddElement(m.elements...)
-	*m = *handler
+func (m *MatrixHandler) Refresh(w, h, sizeCell int) *MatrixHandler {
+	handler := CreateMatrixHandler(w, h, sizeCell)
+	for i := range m.layers {
+		if m.layers[i] == nil {
+			continue
+		}
+		handler.AddElement(m.layers[i].elements...)
+	}
+	return handler
 }
 
 func (m *MatrixHandler) ExecuteOnLayer(layer int, cond func(ele ElementTree) bool) {
-
 	if m.layers[layer] == nil {
 		return
 	}
-	for iy := range m.nRows {
-		for ix := range m.nCollumns {
-			for _, ele := range m.layers[layer].matrix[iy][ix] {
-				if !cond(ele.object) {
-					return
-				}
-			}
+	for _, ele := range m.layers[layer].elements {
+		if !cond(ele) {
+			return
 		}
 	}
+}
+
+func (t *MatrixElement[ElementTree]) isCollidingWithNode(x, y int, width, height int) bool {
+	elementX, elementY := t.object.GetPos()
+	elementWidth, elementHeight := t.object.GetSize()
+	if x >= elementX && x < elementX+elementWidth && y >= elementY && y < elementY+elementHeight {
+		return true
+	}
+	if x+width >= elementX && x+width < elementX+elementWidth && y >= elementY && y < elementY+elementHeight {
+		return true
+	}
+	if x >= elementX && x < elementX+elementWidth && y+height >= elementY && y+height < elementY+elementHeight {
+		return true
+	}
+	if x+width >= elementX && x+width < elementX+elementWidth && y+height >= elementY && y+height < elementY+elementHeight {
+		return true
+	}
+	return false
+}
+
+// to optimize ? maybe with some chacing
+func (m *MatrixHandler) GetCollidingElement(layer int, elementWhichCollides ElementTree) (res []ElementTree) {
+	x, y := elementWhichCollides.GetPos()
+	width, height := elementWhichCollides.GetSize()
+	var tElement MatrixElement[ElementTree]
+	for i := range m.layers[layer].elements {
+		if m.layers[layer].elements[i] == elementWhichCollides {
+			continue
+		}
+		tElement.object = m.layers[layer].elements[i]
+		if tElement.isCollidingWithNode(x, y, width, height) {
+			res = append(res, m.layers[layer].elements[i])
+		}
+	}
+	return res
 }
